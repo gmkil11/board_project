00bffe3a248ca39e59f1851e3f46e68261145e59
gmkil11 <mkkil4451@gmail.com>
2023-11-17 01:36:37 +0900

    파일 업로드 기능

--- src/main/java/org/koreait/commons/MemberUtils.java
+++ src/main/java/org/koreait/commons/MemberUtils.java
@@ -2,13 +2,16 @@ package org.koreait.commons;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.koreait.commons.constants.MemberType;
 import org.koreait.entities.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class MemberUtils {
 
+    @Autowired
     private final HttpSession session;
 
     public Member getMember() {
@@ -19,4 +22,11 @@ public class MemberUtils {
         // null 값이 아니면 로그인 상태다.
         return getMember() != null;
     }
+
+    public boolean isAdmin() {
+        // 어드민인지 확인
+        return isLogin() && getMember().getMtype() == MemberType.ADMIN;
+    }
+
+
 }
